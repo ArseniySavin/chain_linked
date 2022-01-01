28c6811f0590f106b80cad1a0da529459f547817
savin arseniy <a.savin@tha.kz>
2022-01-01 18:26:39 +0600

    clear main

--- main.go
+++ main.go
@@ -7,18 +7,6 @@ func main() {
 		Value: "Hello",
 	}
 
-	chain := Task1Handler{
-		Cfg: Task1Config{Key: "Task1"},
-		Handler: &Task2Handler{
-			Cfg: Task2Config{Key: "Task2"},
-			Handler: &Task3Handler{
-				Cfg: Task3Config{Key: "Task3"},
-			},
-		},
-	}
-
-	chain.Next(domain)
-
 	cfg := Config{
 		TaskCfg1: Task1Config{Key: "Task1"},
 		TaskCfg2: Task2Config{Key: "Task2"},
